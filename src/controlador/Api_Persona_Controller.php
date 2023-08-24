99782b6e27fc40de6da332ae3bf1f163d0926f85
Emanuel Gutierrez <emabarnine@gmail.com>
2023-08-24 20:41:08 -0300

    json 02

--- src/controlador/Api_Persona_Controller.php
+++ src/controlador/Api_Persona_Controller.php
@@ -12,17 +12,40 @@ class Api_Persona_Controller extends Controlador
         $res->lista  = [];
         $res->codigo = 200;
         try {
-            $lista      = Persona::listar();
+            $lista          = Persona::listar();
             $res->respuesta = [
                 "lista" => $lista,
                 "total" => count($lista),
             ];
-            
 
             $this->cargarVista("api/index", $res);
-            
+
         } catch (\Throwable $th) {
-            
+
+            $res->codigo = 500;
+        }
+    }
+
+    public function recibir()
+    {
+        $res         = new stdClass();
+        $res->lista  = [];
+        $res->codigo = 200;
+        try {
+            $datos = file_get_contents("php://input");
+            $json  = json_decode($datos);
+            $clave = $json->clave;
+            $lista = $json->lista;
+
+            $res->respuesta = [
+                "lista" => $lista,
+                "total" => count($lista),
+            ];
+
+            $this->cargarVista("api/index", $res);
+
+        } catch (\Throwable $th) {
+
             $res->codigo = 500;
         }
     }
